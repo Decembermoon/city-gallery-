0e72e993b23aa009f546e9f4c172f103b4bcb482
Decembermoon <44886993+Decembermoon@users.noreply.github.com>
2018-11-09 12:26:43 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,86 @@
+// THE SPACE FOR THE LARGE IMAGE
+let current = document.querySelector('#current');
+
+//WITHOUT LOOP (THE LONG WAY)
+
+const img1=document.getElementById('img1');
+const img2=document.getElementById('img2');
+const img3=document.getElementById('img3');
+const img4=document.getElementById('img4');
+const img5=document.getElementById('img5');
+const img6=document.getElementById('img6');
+const img8=document.getElementById('img7');
+const img8=document.getElementById('img8');
+// img1.addEventListener('click',function(){
+//  current.src = event.target.src;
+// });
+
+// img2.addEventListener('click',function(){
+//   current.src = event.target.src;
+//  });
+ 
+//  img3.addEventListener('click',function(event){
+//   current.src = event.target.src;
+//  });
+ 
+//  img4.addEventListener('click',function(event){
+//   current.src = event.target.src;
+//  });
+ 
+//  img5.addEventListener('click',function(event){
+//   current.src = event.target.src;
+//  });
+ 
+//  img6.addEventListener('click',function(event){
+//   current.src = event.target.src;
+//  });
+ 
+//  img7.addEventListener('click',function(event){
+//   current.src = event.target.src;
+//  });
+ 
+//  img8.addEventListener('click',function(event){
+//   current.src = event.target.src;
+//  });
+ 
+const allImgs = {ima1,img2,img3,img4,
+  img5,img6,img7,img8};
+
+  allimg[0].addEventListener('click',function(event){
+    current.src = event.target.src; 
+  });
+
+  allimg[1].addEventListener('click',function(event){
+    current.src = event.target.src; 
+  });
+
+  allimg[2].addEventListener('click',function(event){
+    current.src = event.target.src; 
+  });
+
+  allimg[3].addEventListener('click',function(event){
+    current.src = event.target.src; 
+  });
+
+  allimg[4].addEventListener('click',function(event){
+    current.src = event.target.src; 
+  });
+
+  allimg[5].addEventListener('click',function(event){
+    current.src = event.target.src; 
+  });
+
+  allimg[6].addEventListener('click',function(event){
+    current.src = event.target.src; 
+  });
+
+  allimg[7].addEventListener('click',function(event){
+    current.src = event.target.src; 
+  });
+
+  allimg[8].addEventListener('click',function(event){
+    current.src = event.target.src; 
+  });
+
+  
+
